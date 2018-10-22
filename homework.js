dc0a85ca6a49683ed04e312d8fc68134242733be
TwentyVT <sonatore@amazon.com>
2018-10-21 20:57:17 -0400

    FINALLY

--- homework.js
+++ homework.js
@@ -1,44 +1,46 @@
-$(document).ready(function () {
 
-var animalArray = ["dog", "cat", "falcon", "iguana", "parrot", "sloth", "lion", "tiger", "bear", "gorrila"]
 
-for (i = 0; i < animalArray.length; i++){
+var animalArray = ["dog", "cat", "falcon", "iguana", "parrot", "sloth", "lion", "tiger", "bear", "gorrilla"]
 
-    var animalBtn = $("<button>")
-    animalBtn.text(animalArray[i]);
-    animalBtn.attr("data-name", animalArray[i]);   
-    $("#button-holder").append(animalBtn)
+function buttonGen () {
+
+$("#button-holder").empty();
+    for (i = 0; i < animalArray.length; i++) {
+        var animalBtn = $("<button>")
+        animalBtn.text(animalArray[i]);
+        animalBtn.attr("data-name", animalArray[i]);   
+        animalBtn.addClass("giffer")
+        $("#button-holder").append(animalBtn)   
+    }
 }
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=w77QS0VOmL8OjHFDNp0MZhC3LVOul8W1&q=" + animal
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
 
-function animalGifMaker(animal) {
+$(document).ready(function () {
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=w77QS0VOmL8OjHFDNp0MZhC3LVOul8W1&q=" + animal
+buttonGen();
+
+$("#button-holder").on("click", ".giffer",  function () {
+
+    var animal = $(this).attr("data-name");
+    console.log(animal)
+    var apiKey = "w77QS0VOmL8OjHFDNp0MZhC3LVOul8W1"
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=10&q=" + animal
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
     });
-
-   
-}
-
-$("#select-artist").on("click", function (event) {
-    // Preventing the button from trying to submit the form
-    event.preventDefault();
-    // Storing the artist name
-    var inputArtist = $("#artist-input").val().trim();
-
-    // Running the searchBandsInTown function(passing in the artist as an argument)
-    searchBandsInTown(inputArtist);
 });
 
-})
+$("#select-animal").on("click", function (event) {
+
+    event.preventDefault();
+    var inputAnimal = $("#animal-spot").val();
+    animalArray.push(inputAnimal)
+    buttonGen();
+});
+
+});
